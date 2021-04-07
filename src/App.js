852a910215ab86c75d94e7f9ba0a9cd25799ed2b
Darren Ladner <darren.ladner@ridetm.org>
2021-04-07 08:41:37 -0500

    finished out react pixel app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Canvas from './components/Canvas';
 import ColorPicker from './components/ColorPicker'
+import React, { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState(0);
   return (
     <div className="App">
-      <ColorPicker />
-      <Canvas />
+      <ColorPicker 
+      currentColor={color}
+      setColor={color => setColor(color)} />
+      <Canvas currentColor={color} />
     </div>
   );
 }
